f6d2d00e677dc401380f306296af9db454b159e3
Jim O'Donnell <jim@zooniverse.org>
2015-03-12 11:42:22 +0000

    List completed subjects with high classification counts on home page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -19,9 +19,9 @@ class WelcomeController < ApplicationController
     scans.each{|sr| @recent_subject_ids << sr.zooniverse_id }
     @recent_subject_ids = @recent_subject_ids.uniq
 
-    # Load 12 most-classificaed subjects
+    # Load 12 most-classified subjects
     @subject_ids = []
-    subs = Subject.where(:zooniverse_id.ne => @tutorial).sort(:classification_count.desc).limit(9)
+    subs = Subject.where(:state => 'complete').sort(:classification_count.desc).limit(9)
     subs.each{|sr| @subject_ids << sr.zooniverse_id }
     @subject_ids = @subject_ids.uniq
 
