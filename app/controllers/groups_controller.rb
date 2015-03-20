11d730f7a1b4583b3e707ec143f5cd106b7596e8
Jim O'Donnell <jim@zooniverse.org>
2015-03-20 14:29:29 +0000

    Better pagination for large groups

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -9,6 +9,8 @@ class GroupsController < ApplicationController
     @page = params[:page].to_i
     @pagetitle = Milkman::Application.config.project["name"]
   	@g ||= Group.find_by_zooniverse_id(params[:zoo_id])
+    @num_pages = @g.stats['total'].to_f / 20
+    @num_pages = @num_pages.ceil
     @next_page = @page + 1
     @prev_page = @page - 1
   end
