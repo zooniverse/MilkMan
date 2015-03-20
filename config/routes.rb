ef22bf36500133eaadc7039f3f3a8c1cfc4aa82f
Jim O'Donnell <jim@zooniverse.org>
2015-03-20 12:35:28 +0000

    Groups Summary stats for groups. Simple paged browsing for group subjects.

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Milkman::Application.routes.draw do
 
   get "subjects/index"
 
-<<<<<<< 7199ffd0884ce79366f6c713713bbf44dd892337
+
+  get 'groups' => 'groups#index'
+  get 'groups/:zoo_id(/:page)' => 'groups#show', defaults: { page: 1 }
   get 'subjects/:zoo_id' => 'subjects#show'
   get 'page/:page_id' => 'subjects#page'
   get 'coordinates' => 'subjects#coordinates'
