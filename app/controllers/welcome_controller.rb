2df62d7d57fd53d8af598631232af4f9292f3c02
Jim O'Donnell <jim@zooniverse.org>
2015-04-09 13:00:38 +0100

    Home page update Add a query to find pages with no illustrations. Display totals for each query (has illustrations, no illustrations, uncertain.)

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -10,8 +10,16 @@ class WelcomeController < ApplicationController
     # Load 12 illustrated subjects
     @illustrated_subject_ids = []
     subjects = Subject.where('metadata.has_illustrations_count' => {:$gte => 10}, :classification_count.lte => 20, :state => 'complete')
-    subjects.limit(9).skip(rand(subjects.size-1)).each{|sr| @illustrated_subject_ids << sr.zooniverse_id }
+    @illustrated_subject_count = subjects.size
+    subjects.limit(9).skip(rand(@illustrated_subject_count-1)).each{|sr| @illustrated_subject_ids << sr.zooniverse_id }
     @illustrated_subject_ids = @illustrated_subject_ids.uniq
+    
+    # no illustrations found
+    @empty_subject_ids = []
+    subjects = Subject.where('metadata.no_illustrations_count' => {:$gte => 3}, :classification_count.lte => 10, :state => 'complete')
+    @empty_subject_count = subjects.size
+    subjects.limit(9).skip(rand(@empty_subject_count-1)).each{|sr| @empty_subject_ids << sr.zooniverse_id }
+    @empty_subject_ids = @empty_subject_ids.uniq
     
     # Load 12 most-recently cached subjects
     @recent_subject_ids = []
@@ -22,7 +30,8 @@ class WelcomeController < ApplicationController
     # Load 9 controversial subjects
     @subject_ids = []
     subs = Subject.where(:classification_count.gt => 20, :state => 'complete')
-    subs.limit(9).skip(rand(subs.size-1)).sort(:classification_count.desc).each{|sr| @subject_ids << sr.zooniverse_id }
+    @subject_count = subs.size
+    subs.limit(9).skip(rand(@subject_count-1)).sort(:classification_count.desc).each{|sr| @subject_ids << sr.zooniverse_id }
     @subject_ids = @subject_ids.uniq
 
     # Preload more if there are not enough preloaded already
